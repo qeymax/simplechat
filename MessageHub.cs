c46c239ec7de50a6863f2b74aab73fcc37476b31
Muhammad <qeymax@gmail.com>
2016-05-03 17:57:34 +0200

    initial

---
+++ MessageHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace simplechat
+{
+    public class MessageHub : Hub
+    {
+        public void Hello(string name ,string message)
+        {
+            Clients.All.Helloo(name , message);
+        }
+    }
+}
---
+++ Startup1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Routing;
+
+[assembly: OwinStartup(typeof(simplechat.Startup1))]
+
+namespace simplechat
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.MapSignalR();
+        }
+
+    }
+}
